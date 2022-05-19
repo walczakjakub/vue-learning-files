395d16335fc6c15f4605bb7b75f325256e7ecc05
Jakub Walczak <jakub_walczak@tlen.pl>
2022-05-19 15:14:00 +0200

    mouseover / mouseleave / dblclick / mousemove

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ const app = Vue.createApp({
       showBooks: true,
       title: 'The Witcher',
       author: 'Andrzej Sapkowski',
-      age: 70
+      age: 70,
+      x: 0,
+      y: 0
     }
   },
   methods: {
-    toggleShowBooks() {
-      this.showBooks = !this.showBooks
-    }
+      toggleShowBooks() {
+        this.showBooks = !this.showBooks
+      },
+      handleEvent(e, data) {
+        console.log(e, e.type)
+        if (data) {
+          console.log(data)
+        }
+      },
+      handleMousemove(e) {
+        this.x = e.offsetX
+        this.y = e.offsetY
+      }
     }
   })
 
